8271d20ec9c396e937ca3128c95cb8fcf817667e
Erdem Agaoglu <erdem.agaoglu@gmail.com>
2020-10-14 17:09:06 +0300

    Revert "Remove mgr livenessProbe"
    This reverts commit 37f43fb99c3b358f4672d60aaf379fe1d58956ee.

--- pkg/operator/ceph/cluster/mgr/spec.go
+++ pkg/operator/ceph/cluster/mgr/spec.go
@@ -34,6 +34,7 @@ import (
 	apps "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 const (
@@ -229,7 +230,16 @@ func (c *Cluster) makeMgrDaemonContainer(mgrConfig *mgrConfig) v1.Container {
 			controller.DaemonEnvVars(c.cephVersion.Image),
 			c.cephMgrOrchestratorModuleEnvs()...,
 		),
-		Resources:       c.resources,
+		Resources: c.resources,
+		LivenessProbe: &v1.Probe{
+			Handler: v1.Handler{
+				HTTPGet: &v1.HTTPGetAction{
+					Path: "/",
+					Port: intstr.FromInt(metricsPort),
+				},
+			},
+			InitialDelaySeconds: 60,
+		},
 		SecurityContext: mon.PodSecurityContext(),
 	}
 
